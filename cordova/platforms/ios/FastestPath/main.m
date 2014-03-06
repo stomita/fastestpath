438c5198c8b83348388a277cc35ed5c736c725d1
Shinichi Tomita <shinichi.tomita@gmail.com>
2014-03-06 14:24:09 +0900

    change main.m

--- cordova/platforms/ios/FastestPath/main.m
+++ cordova/platforms/ios/FastestPath/main.m
@@ -25,11 +25,12 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+#import "SFApplication.h"
 
 int main(int argc, char* argv[])
 {
     @autoreleasepool {
-        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
-        return retVal;
+        return UIApplicationMain(argc, argv, NSStringFromClass([SFApplication class]), NSStringFromClass([AppDelegate class]));
     }
 }
