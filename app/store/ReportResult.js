048272dd4fcdef6e3913be1463e139f7a7d783ec
Shinichi Tomita <shinichi.tomita@gmail.com>
2014-03-08 04:24:51 +0900

    change to scan rows in nested grouped report

--- app/store/ReportResult.js
+++ app/store/ReportResult.js
@@ -16,7 +16,23 @@ Ext.define('FastestPath.store.ReportResult', {
     var conn = jsforce.browser.connection;
     console.log("### Executing " + config.reportId + " ###");
     conn.analytics.report(config.reportId).execute({ details: true }, function(err, result) {
-      var rows = result.factMap["T!T"].rows || [];
+      var factMap = result.factMap;
+      var rows = [];
+      var scanRows = function(groupingD, groupingA) {
+        if (groupingD.groupings.length > 0) {
+          groupingD.groupings.forEach(function(cgroupingD) {
+            scanRows(cgroupingD, groupingA);
+          });
+        } else if (groupingA.groupings.length > 0) {
+          groupingA.groupings.forEach(function(cgroupingA) {
+            scanRows(groupingD, cgroupingA);
+          });
+        } else {
+          var key = [ groupingD.key || "T", groupingA.key || "T" ].join('!');
+          rows.push.apply(rows, factMap[key].rows || []);
+        }
+      };
+      scanRows(result.groupingsDown, result.groupingsAcross);
       var records = rows.map(function(row) {
         var cell = row.dataCells[0];
         return { Name: cell.label, Id: cell.value };
