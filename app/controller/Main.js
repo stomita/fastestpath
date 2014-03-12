3c5260b095c8f985739e832caf8411ebd1d1b708
Shinichi Tomita <shinichi.tomita@gmail.com>
2014-03-12 15:46:26 +0900

    hide bottom tab when scrolling down, show when scroll up

--- app/controller/Main.js
+++ app/controller/Main.js
@@ -3,17 +3,33 @@ Ext.define('FastestPath.controller.Main', {
   extend: 'Ext.app.Controller',
   config: {
     control: {
+      recordListPanel: {
+        scrolldown: 'onListScrollDown',
+        scrollup: 'onListScrollUp'
+      },
       settingButton: {
         tap: 'showSetting'
       }
     },
     refs: {
-      settingDialog: 'setting'
+      mainPanel: 'main',
+      recordListPanel: 'main recordList',
+      settingDialog: 'setting',
     }
   },
 
   showSetting: function() {
     this.getSettingDialog().show();
+  },
+
+  onListScrollDown: function() {
+    var mainTabBar = this.getMainPanel().getTabBar();
+    mainTabBar.hide({ type: 'slideOut', direction: 'down' });
+  },
+
+  onListScrollUp: function() {
+    var mainTabBar = this.getMainPanel().getTabBar();
+    mainTabBar.show({ type: 'slide', direction: 'up' });
   }
 
 });
