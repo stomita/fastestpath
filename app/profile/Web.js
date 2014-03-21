a2121b41e308977be23788a898b4ba825fa6f1d1
Shinichi Tomita <shinichi.tomita@gmail.com>
2014-03-22 01:08:16 +0900

    add client key switch in different environment

--- app/profile/Web.js
+++ app/profile/Web.js
@@ -2,6 +2,20 @@
 Ext.define('FastestPath.profile.Web', {
   extend: 'Ext.app.Profile',
 
+  statics: {
+    jsforceConfig:
+      location.host === 'localhost:1841' ? {
+        clientId: '3MVG9I1kFE5Iul2D_CnNFEpYNxtjSuClAFeKaIA3veILM1b02gF8G9QUCEEDOHeMT643zoeEiZ1QaLgg9.NW8',
+        redirectUri: 'http://localhost:1841/',
+        proxyUrl: 'https://node-salesforce-proxy.herokuapp.com/proxy'
+      } :
+      location.host === 'fastestpath-for-saleforce.herokuapp.com' ? {
+        clientId: '3MVG9I1kFE5Iul2D_CnNFEpYNxgG3_.RT1vX0ycHw1lqt72iTdRm2h6IqzDmBV51kIze5P21WfIEw9On6O18H',
+        redirectUri: 'https://fastestpath-for-salesforce.herokuapp.com/',
+        proxyUrl: 'https://node-salesforce-proxy.herokuapp.com/proxy'
+      } : null
+  },
+
   config: {
     name: 'Web',
     controllers: [
@@ -17,11 +31,8 @@ Ext.define('FastestPath.profile.Web', {
 
   launch: function() {
     var app = this.getApplication();
-    jsforce.browser.init({
-      clientId: '3MVG9I1kFE5Iul2D_CnNFEpYNxtjSuClAFeKaIA3veILM1b02gF8G9QUCEEDOHeMT643zoeEiZ1QaLgg9.NW8',
-      redirectUri: 'http://localhost:1841/',
-      proxyUrl: 'https://node-salesforce-proxy.herokuapp.com/proxy'
-    });
+    jsforce.browser.init(this.statics().jsforceConfig);
+
     var launch = function() {
       var conn = jsforce.browser.connection;
       conn.identity(function(err, res) {
